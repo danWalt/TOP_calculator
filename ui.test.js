4f04d9b0c86aaecdda342dc889ec523b4733587c
daniel walters <daniel.walters2110@gmail.com>
2021-06-01 18:03:46 +0300

    playwright tests working, restructured index.html

--- ui.test.js
+++ ui.test.js
@@ -1,23 +1,7 @@
 const { chromium } = require('playwright');
 import 'babel-polyfill'
+const expect = require('expect');
 
-// (async () => {
-//   //const browser = await chromium.launch({});
-//   const browser = await chromium.launch({headless:false});
-
-//   const page = await browser.newPage();
-//   await page.goto('http://127.0.0.1:5500/index.html');
-//   //await page.screenshot({ path: `example.png` });
-  
-//   await page.click('[id=1]');
-//   let dataScreen = await page.innerText('data-screen')
-   
-//   // run this using node .\ui.test.js
-  
-  
-  
-//   await browser.close();
-// })();
 
 
 describe('open browser', ()=> {
@@ -36,10 +20,159 @@ describe('click on calculator numbers', () => {
         const browser = await chromium.launch({});
         const page = await browser.newPage();
         await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
         await page.click("id=0");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('0');
         await browser.close();
-    })
+    })  
+
+    it('click on 1', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=1");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('1');
+        await browser.close();
+    })  
+
+    it('click on 2', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=2");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('2');
+        await browser.close();
+    })  
+
+    it('click on 3', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=3");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('3');
+        await browser.close();
+    })  
+
+    it('click on 4', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=4");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('4');
+        await browser.close();
+    })  
+
+    it('click on 5', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=5");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('5');
+        await browser.close();
+    })  
+
+    it('click on 6', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=6");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('6');
+        await browser.close();
+    })  
+
+    it('click on 7', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=7");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('7');
+        await browser.close();
+    })  
+
+    it('click on 8', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=8");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('8');
+        await browser.close();
+    })  
+
+    it('click on 9', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=9");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('9');
+        await browser.close();
+    })  
+
 
 
     
+describe ('test calculations', () => {
+    it('add 2 + 5', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=2");
+        await page.click("id=plus");
+        await page.click("id=5");
+        await page.click("id=equals");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('7');
+        await browser.close();
+    })  
+
+
+    it('add 2.3 + 5.41', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=2");
+        await page.click("id=decimal");
+        await page.click("id=3");
+        await page.click("id=plus");
+        await page.click("id=5");
+        await page.click("id=decimal");
+        await page.click("id=4");
+        await page.click("id=1");
+        await page.click("id=plus");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('7.71');
+        await browser.close();
+    })  
+
+
+    it('5.41 - 7.83', async () => {
+        const browser = await chromium.launch({});
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        const dataScreen = await page.$('.data-screen');
+        await page.click("id=5");
+        await page.click("id=decimal");
+        await page.click("id=4");
+        await page.click("id=1");
+        await page.click("id=minus");
+        await page.click("id=7");
+        await page.click("id=decimal");
+        await page.click("id=8");
+        await page.click("id=3");
+        await page.click("id=equals");
+        expect(await dataScreen.evaluate(dataScreen => dataScreen.innerText)).toBe('-2.4199999999999995');
+        await browser.close();
+    })  
+});
+
+   
+
 });
